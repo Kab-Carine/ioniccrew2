3d91bb4cab2e1fa0dbd818dad7c226f5e5e447c0
carine <kabcarine03@gmail.com>
2024-07-08 17:16:18 +0200

    firstIonic

--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -9,17 +9,22 @@ export const routes: Routes = [
       {
         path: 'tab1',
         loadComponent: () =>
-          import('../tab1/tab1.page').then((m) => m.Tab1Page),
+          import('../angular/tab1.page').then((m) => m.Tab1Page),
       },
       {
         path: 'tab2',
         loadComponent: () =>
-          import('../tab2/tab2.page').then((m) => m.Tab2Page),
+          import('../ionic/tab2.page').then((m) => m.Tab2Page),
       },
       {
         path: 'tab3',
         loadComponent: () =>
-          import('../tab3/tab3.page').then((m) => m.Tab3Page),
+          import('../typescript/tab3.page').then((m) => m.Tab3Page),
+      },
+      {
+        path: 'scss',
+        loadComponent: () =>
+          import('../scss/scss.page').then((m) => m.ScssPage),
       },
       {
         path: '',
