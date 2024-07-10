f4e0bbb8492ae0fdf200c5721ec5c5536d0696ad
carine <kabcarine03@gmail.com>
2024-07-10 17:13:34 +0200

    Fin de journée

---
+++ definition.md
@@ -0,0 +1,9 @@
+Definition
+
+*Les composants d'une classe en typescript:
+-le nom de la classe
+-les propriétes
+-un constructeur par défaut
+- getter et setter
+
+*Un constructeur par defaut est un constructeur généré l'or d'une compilation, et ce constructeur peut etre appelé sans paramettre
--- src/app/ionic/tab2.page.ts
+++ src/app/ionic/tab2.page.ts
@@ -1,16 +1,64 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonItem,
+  IonInput,
+  IonLabel,
+  IonRow,
+  IonButton,
+  IonSearchbar
+}
+
+from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { NgFor, NgClass } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    ExploreContainerComponent,
+    IonItem,
+    NgFor,
+    NgClass,
+    IonInput,
+    IonLabel,
+    FormsModule,
+    IonRow,
+    IonButton,
+    IonSearchbar
+  ],
 })
 export class Tab2Page {
+  version: String = '';
 
   constructor() {}
 
+  tableau = this.trierTableau(['ion-header', 'ion-toolbar', 'ion-title', 'ion-segment',
+    'ion-button', 'ion-item', 'ion-list', 'ion-note', 'ion-avatar', 'ion-card']);
+  // tableau: String[] = ['rouge', 'jaune', 'vert', 'bleu']
+  tableauTrie: String[] = [];
+
+  trierTableau(tableau: String[]): String[] {
+    tableau.sort();
+    return tableau;
+  }
+
+  fun(){
+    this.tableau.push(this.version);
+    this.tableau.sort();
+    return this.tableau;
+  }
 }
